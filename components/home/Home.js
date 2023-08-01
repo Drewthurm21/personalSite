0124845962f3365b2b1e8ff3a2c1af881d1cec1c
Drewthurm21 <drewthurm21@gmail.com>
2023-08-01 14:36:33 -0500

    added nav folder and sidebar file / scss module. added styles for sidebar

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './home.module.scss'
-
+import { SideBar } from '../nav/SideBar';
 
 export default function Home() {
 
   return (
     <>
       <div className={styles.home}>
-        <div>
-          sidebar
-        </div>
+        <SideBar />
         <main>
           main section
         </main>
---
+++ components/nav/SideBar.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import styles from "./sidebar.module.scss";
+import { motion } from "framer-motion";
+
+const linkOptions = { initial: { x: -70 }, animate: { x: 0 } }
+const myLinksLabels = ['about', 'projects', 'experience', 'contact']
+
+export const SideBar = () => {
+  const [selected, setSelected] = useState("");
+
+  return (
+    <motion.nav
+      {...linkOptions}
+      transition={{ duration: 0.5 }}
+      className={styles.sideBar}
+    >
+      <span className={styles.logo}>
+        d <span>t</span>
+      </span>
+      {myLinksLabels.map((label, i) => (
+        <motion.a {...linkOptions} href={`#${label}`}
+          onClick={() => setSelected(label)}
+          transition={{ duration: 0.5, delay: (i / 10) }}
+          className={selected === label ? styles.selected : ""}
+        >
+          {label[0].toLocaleUpperCase() + label.slice(1)}
+        </motion.a>
+      ))
+      }
+    </motion.nav >
+  );
+};
