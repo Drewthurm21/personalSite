af7f26dd30306ccd32b5fb90e472ea385dc93363
Drewthurm21 <drewthurm21@gmail.com>
2023-08-02 22:36:21 -0500

    added experience section

---
+++ components/home/experience/ExpItem.js
@@ -0,0 +1,40 @@
+import RevealWrap from "@/components/utils/RevealWrap";
+import styles from "./expsection.module.scss";
+
+export const ExperienceItem = ({
+  title, position, time, location, description, tech,
+}) => {
+  return (
+    <div className={styles.experience}>
+      <div className={styles.header}>
+        <RevealWrap>
+          <span className={styles.title}>{title}</span>
+        </RevealWrap>
+        <RevealWrap>
+          <span>{time}</span>
+        </RevealWrap>
+      </div>
+
+      <div className={styles.header}>
+        <RevealWrap>
+          <span className={styles.position}>{position}</span>
+        </RevealWrap>
+        <RevealWrap>
+          <span>{location}</span>
+        </RevealWrap>
+      </div>
+      <RevealWrap>
+        <p className={styles.description}>{description}</p>
+      </RevealWrap>
+      <RevealWrap>
+        <div className={styles.tech}>
+          {tech.map((item) => (
+            <span key={item} className="chip">
+              {item}
+            </span>
+          ))}
+        </div>
+      </RevealWrap>
+    </div>
+  );
+};
--- components/home/experience/ExpSection.js
+++ components/home/experience/ExpSection.js
@@ -1,12 +1,11 @@
 import { SectionHeader } from "@/components/utils/SectionHeader";
-import { ExperienceItem } from "./ExperienceItem";
 
 export const Experience = () => {
   return (
     <section className="section-wrapper" id="experience">
       <SectionHeader title="Experience" dir="l" />
       {experience.map((item) => (
-        <ExperienceItem key={item.title} {...item} />
+        <div> Yo </div>
       ))}
     </section>
   );
