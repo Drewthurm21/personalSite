ea3b898d0742a213ab834373a3b10dc6511a19dd
Drewthurm21 <drewthurm21@gmail.com>
2023-08-02 19:58:23 -0500

    tweaked header and globals to add eamil in header w/o clipping

--- components/nav/header/SiteHeader.js
+++ components/nav/header/SiteHeader.js
@@ -7,6 +7,7 @@ export default function SiteHeader() {
   return (
     <header className={styles.header} >
       <HeaderLinks />
+      <p>drewthurmcodes@gmail.com</p>
       <OutlinedButton onClick={() => window.open("/myResume.pdf")}>My Resume</OutlinedButton>
     </header>
   )
