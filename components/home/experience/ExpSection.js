2e3c7071d7d911d5e0fd847846bece94a60a1275
Drewthurm21 <drewthurm21@gmail.com>
2023-08-03 14:46:42 -0500

    fixing typos in exp section

--- components/home/experience/ExpSection.js
+++ components/home/experience/ExpSection.js
@@ -19,8 +19,8 @@ const experience = [
     time: "Contract",
     location: "Overland Park, KS",
     description:
-      "Built API to calculate job costs & quote services on site/on the fly. Made a pretty front-end in React. Also tagged along for a power washing adventure.",
-    tech: ['Node', 'React', 'Redux', 'Tailwind', 'SCSS', 'Render', 'Supabase'],
+      "Built a database and API to track inventory, calculate job costs & quote services on-site/on the fly. Made a pretty front-end in React for it. Tagged along for a wash & seal adventure. Turns out, concrete is kinda cool!",
+    tech: ['Python', 'Flask', 'SQLAlchemy', 'Render', 'Node.js', 'React', 'Redux', 'Tailwind', 'SCSS', 'Framer Motion'],
   },
   {
     title: "CottageCare",
@@ -28,7 +28,7 @@ const experience = [
     time: "2021 - 2023",
     location: "Remote",
     description:
-      "Designed and built web applications responsible for quoting and scheduling customers. Automated call center reps' clerical work by integrating 3rd party APIs. Also went on a hike! It was fun.",
+      "Designed and built web applications responsible for quoting and scheduling customers. Automated call center reps' clerical work. Explored a national park with co-workers. It was fun!",
     tech: [
       'Node', 'React', 'Redux', 'CSS', 'Python', 'Flask', 'SQLAlchemy', 'Heroku',
       'Email.JS', 'Kendo PDF', 'ActiveCampaign', 'AWS', 'Docker', 'GitHub'
@@ -40,7 +40,7 @@ const experience = [
     time: "2021 - 2023",
     location: "Remote",
     description:
-      "Lead cohorts of 90-120 students through a rigorous 1000hr coding bootcamp, averaging an 82% grad rate. Constructed code-along project and curated lectures. Met with Kush one time. He said he liked Zoom background. I said I liked his hair.",
+      "Lead cohorts of 90-120 students through a rigorous 1000-hour coding bootcamp, averaging an 82% grad rate. Constructed code-along projects and curated lectures. Met with Kush one time. He said he liked my Zoom background. I said I liked his hair.",
     tech: [
       'HTML', 'CSS', 'Node', 'JavaScript', 'AJAX', 'React', 'Redux', 'Express',
       'Sequelize', 'PostgresQL', 'SQLite', 'Python', 'Flask', 'SQLAlchemy',
