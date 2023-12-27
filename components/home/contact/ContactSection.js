3871559958359223815ff31123c5f6a4609737ed
Drewthurm21 <drewthurm21@gmail.com>
2023-12-27 12:37:54 -0600

    added and styled terminal contact form

--- components/home/contact/ContactSection.js
+++ components/home/contact/ContactSection.js
@@ -1,40 +1,15 @@
 import RevealWrap from "@/components/utils/RevealWrap";
+import ContactForm from "@/components/home/contact/ContactForm";
 import { AiFillMail, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import Link from "next/link";
 import styles from "./contactsection.module.scss";
+import SectionHeader from "@/components/utils/SectionHeader";
 
 export default function ContactSection() {
   return (
     <section className="section-wrapper" id="contact">
-      <div className={styles.contactWrapper}>
-        <RevealWrap width="100%">
-          <h4 className={styles.contactTitle}>
-            Contact<span>.</span>
-          </h4>
-        </RevealWrap>
-        <RevealWrap width="100%">
-          <div>
-            Here are my links if you want to connect.
-          </div>
-          <div>
-            I&apos;d love to hear from you!
-          </div>
-          <Link href="mailto:drewthurmcodes@gmail.com" target="_blank">
-            <span className={styles.contactEmail}>drewthurmcodes@gmail.com</span>
-          </Link>
-          <div className={styles.contactIcons}>
-            <Link href="mailto:drewthurmcodes@gmail.com" target="_blank">
-              <AiFillMail size="2.4rem" />
-            </Link>
-            <Link href="https://www.github.com/drewthurm21" target="_blank">
-              <AiFillGithub size="2.4rem" />
-            </Link>
-            <Link href="https://www.linkedin.com/in/drew-thurman/" target="_blank">
-              <AiFillLinkedin size="2.4rem" />
-            </Link>
-          </div>
-        </RevealWrap>
-      </div>
+      <SectionHeader title="Contact" dir="r" />
+      <ContactForm />
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./components/home/contact/ContactForm.js'],
   theme: {
     extend: {
       fontFamily: {
         sans: [`var(--font-poppins)`],
       },
     },
+
   },
   plugins: [],
 }
