c4404f28dcf32bebd7eec88a464714fcf58db456
Drewthurm21 <drewthurm21@gmail.com>
2023-12-27 10:51:34 -0600

    added poppins font variable to tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import '@/styles/globals.css'
 import { Poppins } from 'next/font/google'
 
-const poppins = Poppins({ weight: ["100", "200", "400", "700", "900"], subsets: ['latin'] })
+const poppins = Poppins({
+  weight: ["100", "200", "400", "700", "900"],
+  subsets: ['latin'],
+  variable: '--font-poppins',
+})
 
 export default function App({ Component, pageProps }) {
   return (
-    <div id='root' className={poppins.className}>
+    <div id='root' className={`${poppins.className} font-sans`}>
       <Component {...pageProps} />
     </div>
   )
-}
+} 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [`var(--font-poppins)`],
+      },
+    },
   },
   plugins: [],
 }
