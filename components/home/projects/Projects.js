1d1f9abc20c6b21d8467690503834729634b4d72
Drewthurm21 <drewthurm21@gmail.com>
2024-01-18 00:19:48 -0600

    added Sorted app to projects. updated resume file

--- components/home/projects/Projects.js
+++ components/home/projects/Projects.js
@@ -23,18 +23,56 @@ const projects = [
     code: "https://github.com/Drewthurm21/jakeprofile",
     projectLink: "https://www.jakehembree.com",
     tech: ["Next.js", "React", "Tailwind", "Framer-Motion", "Resend", "Vercel"],
-    description: "Are you looking for a portfolio website for your small business or company?  I can do that!",
+    description: "Are you in need of an online portfolio, or a website for your small business?  I can do that!",
     modalContent: (
       <>
         <p>
           One of my favorite projects so far! This is a beautiful portfolio website with a simple built-in CMS for my friend, and insanely talented graphic designer, Jake Hembree.
         </p>
         <p>
-          Since this is a simple SPA with minimal interactivity, I decided to build this in Next.js / React using Tailwind and framer-motion for the styles. The email functionality and templating are handled by Resend, and
+          It's built in Next.js / React using Tailwind and framer-motion for the styles. The email functionality and templating are handled by Resend, and
           the site is deployed on Render.
         </p>
         <p>
           He wanted a simple, clean, easy to update site to showcase his work, and I think we nailed it! Check it out via the link below.
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "Sorted: A Sorting Algorithm Visualizer",
+    imgSrc: "../../../sortedHero.png",
+    code: "https://github.com/Drewthurm21/sortedapp",
+    projectLink: "https://sortedapp.vercel.app/",
+    tech: ["Next.js", "React", "Tailwind", "Framer-Motion", "Vercel", "Algorithms"],
+    description: "Ever wondered what the machine elves actually doing inside your PC? I explain that here.",
+    modalContent: (
+      <>
+        <p>
+          An app I made to help my students learn sorting algorithms by seeing them work in real time.
+          I&apos;ve written articles covering the strategy behind each algorithm, and the tradeoffs we make when selecting them.
+          It&apos;s a great way to learn about how sorting algorithms work, and how they differ from one another.
+        </p>
+        <p>
+          The tech stack? Next.js with React using context for state management. I chose Tailwind for styling, and framer-motion for animations. It&apos;s deployed on Vercel.
+        </p>
+        <p>
+          If you&apos;re interested in how something like this is built I&apos;ve also created a dev notes blog. It covers my thoughts on implementations and tradeoffs I consider while building.
+          It&apos;s in the wiki of the github repo. Check it out!
+        </p>
+        <p>
+          So far I&apos;ve completed:
+          <p>Dev notes blog started in wiki</p>
+          <p>Bubble sort article and visualizer completed</p>
+          <p>Insertion sort article</p>
+        </p>
+        <p>
+          Features coming soon:
+          <p>Visualizer for insertion sort</p>
+          <p>Article and visualizer for Merge Sort, </p>
+          <p>Article and graphs for time and space complexity - BigO</p>
+          <p>*Expand to include search algos?</p>
+          <p>*Expand to include pathing algos?</p>
         </p>
       </>
     ),
@@ -45,7 +83,7 @@ const projects = [
     code: "https://github.com/Drewthurm21/littleJohn",
     projectLink: "",
     tech: ["Node.js", "React", "Redux", "Python", "Flask", "SQLAlchemy", "APIs", "Socket.io"],
-    description: "Have you ever wondered how it would feel to trade millions from the comfort of your own couch?  You might wanna try this app I made.",
+    description: "Wanna know what it's like to trade millions from the comfort of your own couch? I made an app for that.",
     modalContent: (
       <>
         <p>
